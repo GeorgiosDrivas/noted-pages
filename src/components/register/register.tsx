ec3b50364bf99a34f56f8e456db69e38136dac83
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2024-10-09 15:24:36 +0300

    Back to home page button

--- src/components/register/register.tsx
+++ src/components/register/register.tsx
@@ -1,34 +1,40 @@
+import { Link } from "react-router-dom";
 import "./register.css";
 
 export default function Register() {
   return (
     <>
-      <div className="container-fluid">
-        <div className="row h-100">
-          <div className="col-12 p-0">
-            <div className="wrap w-100 h-100 d-flex justify-content-center align-items-center">
-              <div id="register-board">
-                <h1 className="text-center">Sign up for free!</h1>
-                <form>
-                  <div>
-                    <label htmlFor="name">Full name</label>
-                    <input type="text" id="name" />
-                  </div>
-                  <div>
-                    <label htmlFor="username">Username</label>
-                    <input type="text" id="username" />
-                  </div>
-                  <div>
-                    <label htmlFor="password">Password</label>
-                    <input type="text" id="password" />
-                  </div>
-                  <button type="submit">Sign up</button>
-                </form>
+      <main className="h-100 w-100">
+        <Link to="/" className="ms-3">
+          Home
+        </Link>
+        <div className="container-fluid">
+          <div className="row h-100">
+            <div className="col-12 p-0">
+              <div className="wrap w-100 h-100 d-flex justify-content-center align-items-center">
+                <div id="register-board">
+                  <h1 className="text-center">Sign up for free!</h1>
+                  <form>
+                    <div>
+                      <label htmlFor="name">Full name</label>
+                      <input type="text" id="name" />
+                    </div>
+                    <div>
+                      <label htmlFor="username">Username</label>
+                      <input type="text" id="username" />
+                    </div>
+                    <div>
+                      <label htmlFor="password">Password</label>
+                      <input type="text" id="password" />
+                    </div>
+                    <button type="submit">Sign up</button>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </main>
     </>
   );
 }
--- src/components/sign-in/signIn.tsx
+++ src/components/sign-in/signIn.tsx
@@ -4,32 +4,37 @@ import "./signIn.css";
 export default function SignIn() {
   return (
     <>
-      <div className="container-fluid">
-        <div className="row h-100">
-          <div className="col-12 p-0">
-            <div className="wrap h-100 w-100 d-flex justify-content-center align-items-center">
-              <div id="board">
-                <h1 className="text-center">Welcome back!</h1>
-                <form>
-                  <div>
-                    <label htmlFor="username">Username</label>
-                    <input type="text" id="username" />
-                  </div>
-                  <div>
-                    <label htmlFor="password">Password</label>
-                    <input type="text" id="password" />
-                  </div>
-                  <button type="submit">Sign in</button>
-                </form>
-                <p className="text-center mt-2">
-                  Don't have an account? <Link to="/sign-up">Sign up</Link> for
-                  free!
-                </p>
+      <main className="h-100 w-100">
+        <Link to="/" className="ms-3">
+          Home
+        </Link>
+        <div className="container-fluid">
+          <div className="row h-100">
+            <div className="col-12 p-0">
+              <div className="wrap h-100 w-100 d-flex justify-content-center align-items-center">
+                <div id="board">
+                  <h1 className="text-center">Welcome back!</h1>
+                  <form>
+                    <div>
+                      <label htmlFor="username">Username</label>
+                      <input type="text" id="username" />
+                    </div>
+                    <div>
+                      <label htmlFor="password">Password</label>
+                      <input type="text" id="password" />
+                    </div>
+                    <button type="submit">Sign in</button>
+                  </form>
+                  <p className="text-center mt-2">
+                    Don't have an account? <Link to="/sign-up">Sign up</Link>{" "}
+                    for free!
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </main>
     </>
   );
 }
