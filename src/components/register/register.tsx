faa8b2d633151bb00663007da0a84b167c4b0eab
GeorgiosDrivas <drivasgeorgios0@gmail.com>
2024-10-10 14:10:09 +0300

    Register functionality

--- src/components/register/register.tsx
+++ src/components/register/register.tsx
@@ -1,7 +1,30 @@
 import "../../styles/auth.css";
+import { addUserUrl } from "../../url";
 import BacktoHome from "../backToHome";
+import { useState } from "react";
 
 export default function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [fullname, setFullname] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    fetch(addUserUrl, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+        fullname: fullname,
+      }),
+    });
+  };
+
   return (
     <>
       <main className="h-100 w-100">
@@ -15,18 +38,36 @@ export default function Register() {
                   className="d-flex justify-content-center flex-column"
                 >
                   <h1 className="text-center">Sign up for free!</h1>
-                  <form className="d-flex flex-column justify-content-center align-items-center">
+                  <form
+                    onSubmit={(e) => handleSubmit(e)}
+                    className="d-flex flex-column justify-content-center align-items-center"
+                  >
                     <div>
                       <label htmlFor="name">Full name</label>
-                      <input type="text" id="name" />
+                      <input
+                        type="text"
+                        id="name"
+                        value={fullname}
+                        onChange={(e) => setFullname(e.target.value)}
+                      />
                     </div>
                     <div>
                       <label htmlFor="username">Username</label>
-                      <input type="text" id="username" />
+                      <input
+                        type="text"
+                        id="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                      />
                     </div>
                     <div>
                       <label htmlFor="password">Password</label>
-                      <input type="text" id="password" />
+                      <input
+                        type="text"
+                        id="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                      />
                     </div>
                     <button type="submit">Sign up</button>
                   </form>
---
+++ src/url.ts
@@ -0,0 +1,1 @@
+export const addUserUrl = "http://localhost:8080/users/add-user";
